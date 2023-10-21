e4a4343e4175a8a47a58f4cfe0c930f2e02826ac
ZviezdilinArt <118184844+ZviezdilinArt@users.noreply.github.com>
2023-10-21 19:25:04 +0300

    correction patterns

--- src/components/Phonebook1/Phonebook.jsx
+++ src/components/Phonebook1/Phonebook.jsx
@@ -53,7 +53,7 @@ export const Phonebook = () => {
             type="text"
             name="name"
             value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             onChange={handleOnChange}
@@ -65,7 +65,7 @@ export const Phonebook = () => {
             type="tel"
             name="number"
             value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             onChange={handleOnChange}
